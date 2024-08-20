6c73a30e046828d537bd312eec05ab60330f7b11
msaufi2325 <msaufi2325@gmail.com>
2024-08-21 04:36:22 +0900

    Update App component layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import TodoItem from "./components/TodoItem"
 function App() {
 
   return (
-    <main className="py-10 h-screen">
-      <h1 className="font-bold text-3x1 text-center">Your Todos</h1>
-      <div className="max-w-lg mx-auto">
+    <main className="py-10 h-screen space-y-5">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {dummyData.map((todo) => (
             <TodoItem todo={todo} />
