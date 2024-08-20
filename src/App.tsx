38e7070e55eb4b5e8d441d09030d7543d8e8187b
msaufi2325 <msaufi2325@gmail.com>
2024-08-21 04:08:04 +0900

    Add dummy todo data to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { dummyData } from "./assets/data/todo"
 
 function App() {
 
   return (
     <main className="py-10 h-screen">
       <h1 className="font-bold text-3x1 text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <p key={todo.id} className="text-lg">
+              {todo.title}
+
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
