c08f3368f1986edea9b07d3568291065626ff256
msaufi2325 <msaufi2325@gmail.com>
2024-08-22 06:40:47 +0900

    Refactor App component and useTodos hook for better code organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,16 @@
-import { useState } from "react"
-import { dummyData } from "./assets/data/todo"
-import { Todo } from "./types/todo"
 import AddTodoForm from "./components/AddTodoForm"
 import TodoList from "./components/TodoList"
 import TodoSummary from "./components/TodoSummary"
+import useTodos from "./hooks/useTodos"
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>(dummyData)
-
-  function setTodoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodos) => 
-      prevTodos.map((todo) => (todo.id === id ? { ...todo,completed } : todo))
-    )
-  }
-
-  function addTodo (title: string) {
-    setTodos((prevTodos) => [
-      {
-        id: Math.max(...prevTodos.map((todo) => todo.id), 0) + 1,
-        title,
-        completed: false
-      },
-      ...prevTodos,
-    ]);
-  }
-
-  function deleteTodo (id: number) {
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
-  }
-
-  function deleteAllCompletedTodos() {
-    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed))
-  }
+  const {
+    todos,
+    addTodo,
+    setTodoCompleted,
+    deleteTodo,
+    deleteAllCompletedTodos
+  } = useTodos()
 
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
